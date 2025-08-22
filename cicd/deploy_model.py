6b8da58e83ba9390100bcbd7c3a2fc179c897a18
Abdullah Shahzad <abdullahhunjra@gmail.com>
2025-08-22 12:38:06 +0100

    Model Deployment Commit

--- cicd/deploy_model.py
+++ cicd/deploy_model.py
@@ -6,8 +6,11 @@ from datetime import datetime
 # --- CONFIG ---
 role = "arn:aws:iam::755283537318:role/telco-sagemaker-role"
 region = "us-east-1"
+bucket = "rossmann-sales-bucket"
+output_prefix = "rf-hpo-output"
 tuning_job_name = "rf-hpo-2025-08-21-19-05-05"
 
+# --- SageMaker session and client ---
 session = sagemaker.Session()
 sm_client = boto3.client("sagemaker", region_name=region)
 
@@ -18,14 +21,16 @@ tuning_info = sm_client.describe_hyper_parameter_tuning_job(
 best_job_name = tuning_info["BestTrainingJob"]["TrainingJobName"]
 print(f"✅ Best training job: {best_job_name}")
 
-# --- Step 2: Get model artifact + training image ---
-job_info = sm_client.describe_training_job(TrainingJobName=best_job_name)
-model_artifact = job_info["ModelArtifacts"]["S3ModelArtifacts"]
-image_uri = job_info["AlgorithmSpecification"]["TrainingImage"]
-print(f"✅ Model artifact: {model_artifact}")
-print(f"✅ Training image: {image_uri}")
+# --- Step 2: Construct S3 model artifact path ---
+model_artifact = f"s3://{bucket}/{output_prefix}/{best_job_name}/output/model.tar.gz"
+print(f"✅ Constructed model path: {model_artifact}")
 
-# --- Step 3: Register and deploy model ----
+# --- Step 3: Get training image used by that job ---
+training_info = sm_client.describe_training_job(TrainingJobName=best_job_name)
+image_uri = training_info["AlgorithmSpecification"]["TrainingImage"]
+print(f"✅ Training image URI: {image_uri}")
+
+# --- Step 4: Register and deploy model ---
 model = Model(
     image_uri=image_uri,
     model_data=model_artifact,
@@ -33,11 +38,11 @@ model = Model(
     sagemaker_session=session
 )
 
-endpoint_name = f"rossmann-endpoint-{datetime.now().strftime('%Y%m%d%H%M%S')}"
+endpoint_name = f"rossmann-store-sales-endpoints"
 predictor = model.deploy(
     initial_instance_count=1,
     instance_type="ml.m5.large",
     endpoint_name=endpoint_name
 )
 
-print(f"✅ Model deployed at endpoint: {endpoint_name}")
+print(f"✅ Model deployed to endpoint: {endpoint_name}")
